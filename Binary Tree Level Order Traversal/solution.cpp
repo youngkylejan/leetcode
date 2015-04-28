af71ccb6f0acff732836471afc36853dce44650d
kylejan <765108938@qq.com>
2015-04-28 23:25:22 +0800

    change format

--- Binary Tree Level Order Traversal/solution.cpp
+++ Binary Tree Level Order Traversal/solution.cpp
@@ -9,19 +9,19 @@
  */
 class Solution {
 public:
-	void DFS(TreeNode *root, int depth, vector<vector<int>>& result) {
-		if (!root) {
-			return;
-		}
+    void DFS(TreeNode *root, int depth, vector<vector<int>>& result) {
+        if (!root) {
+            return;
+        }
 
-		if (depth == result.size()) {
-			result.push_back(vector<int>());
-		}
+        if (depth == result.size()) {
+            result.push_back(vector<int>());
+        }
 
-		result[depth].push_back(root->val);
-		DFS(root->left, depth + 1, result);
-		DFS(root->right, depth + 1, result);
-	}
+        result[depth].push_back(root->val);
+        DFS(root->left, depth + 1, result);
+        DFS(root->right, depth + 1, result);
+    }
 
     vector<vector<int> > levelOrder(TreeNode *root) {
         vector<vector<int>> result;
