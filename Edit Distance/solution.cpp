29d497414fad8a5a33b0fe2e8329076d74cafffe
kylejan <765108938@qq.com>
2015-05-01 14:40:10 +0800

    add cpp solution

--- Edit Distance/solution.cpp
+++ Edit Distance/solution.cpp
@@ -2,5 +2,20 @@ class Solution {
 public:
     int minDistance(string word1, string word2) {
         
+        vector<vector<int>> dp = vector<vector<int>>(word1.length() + 1, vector<int>(word2.length() + 1, 0));
+
+        for (int i = 0; i <= word1.length(); i++)
+            dp[i][0] = i;
+
+        for (int j = 0; j <= word2.length(); j++)
+            dp[0][j] = j;
+
+        for (int i = 1; i <= word1.length(); i++)
+            for (int j = 1; j <= word2.length(); j++) {
+                dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1);
+                dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + ((word1[i - 1] == word2[j - 1]) ? 0 : 1));
+            }
+
+        return dp[word1.length()][word2.length()];
     }
 };
